5573f3b82982cf26a81a9d314a6971f4aa30565b
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-07-18 15:27:08 +0200

    remove navbar for now

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import MainChat from "@/components/MainChat";
 import Sidebar from "@/components/Sidebar";
 
-
 export default function Home() {
   return (
     <div className="h-screen grid desktop:grid-cols-12 desktop:auto-rows-auto">
-      <Sidebar />
       <MainChat />
     </div>
   );
