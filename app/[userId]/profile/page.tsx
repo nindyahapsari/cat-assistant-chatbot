bfee5bb738de48b680d9df91277ec057c25b8fd6
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-07-31 01:12:40 +0200

    connect to db for user

--- app/[userId]/profile/page.tsx
+++ app/[userId]/profile/page.tsx
@@ -3,7 +3,6 @@ import { UserProfile, useUser } from "@clerk/nextjs";
 
 export default function Profile() {
   const { user } = useUser();
-  console.log("profile user", user);
   return (
     <div className="h-screen flex justify-center items-center">
       <UserProfile />
