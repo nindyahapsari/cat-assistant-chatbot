dacb21f686facab5e85b75f715ef79c5eb7b6c8e
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-08-01 22:52:44 +0200

    wip: sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useEffect } from "react";
 import {
   SignInButton,
   SignedIn,
@@ -8,11 +9,39 @@ import {
   UserButton,
   SignUpButton,
 } from "@clerk/nextjs";
-import { Button } from "./ui/button";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
-import { useEffect } from "react";
 import Image from "next/image";
+import { Button } from "./ui/button";
+import {
+  Bell,
+  CircleUser,
+  Home,
+  LineChart,
+  Menu,
+  Package,
+  Package2,
+  Search,
+  ShoppingCart,
+  Users,
+} from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 export default function Sidebar() {
   const { user } = useUser();
@@ -21,33 +50,147 @@ export default function Sidebar() {
 
   const userId = user?.id;
 
-  return (
-    <>
-      <div className="bg-slate-100 h-full px-12  flex flex-col items-center desktop:col-span-2 desktop:row-span-12">
-        <div className="my-2">
-          <Link href="/">
-            <Image
-              alt="Product image"
-              className="aspect-square w-full rounded-md object-cover"
-              src="/assets/1.svg"
-              height="300"
-              width="300"
-            />
-          </Link>
-        </div>
-        <div className="my-8 h-full w-full flex flex-col justify-end items-center gap-4">
-          <SignedOut>
-            <SignInButton mode="modal">
-              <Button>Sign In</Button>
-            </SignInButton>
-          </SignedOut>
+  // return (
+  //     <div className="bg-slate-100 h-full px-12  flex flex-col items-center desktop:col-span-2 desktop:row-span-12">
+  //       <div className="my-2">
+  //         <Link href="/">
+  //           <Image
+  //             alt="Product image"
+  //             className="aspect-square w-full rounded-md object-cover"
+  //             src="/assets/1.svg"
+  //             height="300"
+  //             width="300"
+  //           />
+  //         </Link>
+  //       </div>
+  //       <div className="my-8 h-full w-full flex flex-col justify-end items-center gap-4">
+  //         <SignedOut>
+  //           <SignInButton mode="modal">
+  //             <Button>Sign In</Button>
+  //           </SignInButton>
+  //         </SignedOut>
 
-          <SignedIn>
-            <Link href={`/cat-profile`}>Cat Profile</Link>
-            <UserButton />
-          </SignedIn>
+  //         <SignedIn>
+  //           <Link href={`/cat-profile`}>Cat Profile</Link>
+  //           <UserButton />
+  //         </SignedIn>
+  //       </div>
+  //     </div>
+  // );
+
+  return (
+    <div className="grid min-h-screen w-full tablet:grid-cols-[220px_1fr] desktop:grid-cols-[280px_1fr]">
+      <div className="hidden border-r bg-muted/40 tablet:block">
+        <div className="flex h-full max-h-screen flex-col gap-2">
+          <div className="flex h-14 items-center border-b px-4 desktop:h-[60px] desktop:px-6">
+            <Link href="/" className="flex items-center gap-2 font-semibold">
+              <Package2 className="h-6 w-6" />
+              <span className="">Acme Inc</span>
+            </Link>
+          </div>
+          <div className="flex-1">
+            <nav className="grid items-start px-2 text-sm font-medium desktop:px-4">
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <Home className="h-4 w-4" />
+                Dashboard
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <ShoppingCart className="h-4 w-4" />
+                Orders
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
+              >
+                <Package className="h-4 w-4" />
+                Products{" "}
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <Users className="h-4 w-4" />
+                Customers
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <LineChart className="h-4 w-4" />
+                Analytics
+              </Link>
+            </nav>
+          </div>
         </div>
       </div>
-    </>
+      <div className="flex flex-col">
+        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 tablet:hidden desktop:h-[60px] desktop:px-6 desktop:hidden">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                variant="outline"
+                size="icon"
+                className="shrink-0 tablet:hidden desktop:hidden"
+              >
+                <Menu className="h-5 w-5 " />
+                <span className="sr-only">Toggle navigation menu</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="flex flex-col">
+              <nav className="grid gap-2 text-lg font-medium">
+                <Link
+                  href="#"
+                  className="flex items-center gap-2 text-lg font-semibold"
+                >
+                  <Package2 className="h-6 w-6" />
+                  <span className="sr-only">Acme Inc</span>
+                </Link>
+                <Link
+                  href="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                >
+                  <Home className="h-5 w-5" />
+                  Dashboard
+                </Link>
+                <Link
+                  href="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
+                >
+                  <ShoppingCart className="h-5 w-5" />
+                  Orders
+                </Link>
+                <Link
+                  href="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                >
+                  <Package className="h-5 w-5" />
+                  Products
+                </Link>
+                <Link
+                  href="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                >
+                  <Users className="h-5 w-5" />
+                  Customers
+                </Link>
+                <Link
+                  href="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                >
+                  <LineChart className="h-5 w-5" />
+                  Analytics
+                </Link>
+              </nav>
+            </SheetContent>
+          </Sheet>
+        </header>
+      </div>
+    </div>
   );
 }
