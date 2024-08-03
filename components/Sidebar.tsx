e521b0a6ab4d4c638a29cb07cddcfbe0c1656d81
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-08-03 22:05:35 +0200

    refactor: Improve responsiveness and scrolling behavior in Sidebar and MainChat components

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -42,7 +42,7 @@ export default function Sidebar({ children }: { children: ReactNode }) {
   }
 
   return (
-    <div className="grid max-h-screen w-full tablet:grid-cols-[220px_1fr] desktop:grid-cols-[280px_1fr]">
+    <div className="grid  w-full tablet:grid-cols-[220px_1fr] desktop:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 tablet:block">
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 desktop:h-[100px] desktop:px-6">
@@ -56,11 +56,11 @@ export default function Sidebar({ children }: { children: ReactNode }) {
         </div>
       </div>
       <div className="flex flex-col">
-        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 mobile:max-w-screen desktop:h-[60px] desktop:px-6">
+        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 mobile:max-w-scree desktop:h-[60px] desktop:px-6">
           <HeaderElements sidebarLinksInfo={SIDEBARLINKSINFO} />
         </header>
-        <main className="mobile:max-w-[375px] flex flex-1 flex-col gap-4 p-4 tablet:max-w-[1440px] tablet:max-h-[calc(100vh-60px)]">
-          <div className="mobile:overflow-scroll tablet:overflow-auto tablet:w-[calc(90vw-220px)] desktop:w-[calc(95vw-280px)]">
+        <main className="mobile:max-w-[375px] overflow-hidden flex flex-1 flex-col gap-4 p-4 tablet:max-w-[1440px] tablet:max-h-[calc(100vh-60px)]">
+          <div className="mobile:overflow-scroll tablet:overflow-auto tablet:overflow-y-scroll tablet:w-[calc(90vw-220px)] desktop:w-[calc(95vw-280px)]">
             {children}
           </div>
         </main>
