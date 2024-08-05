4dbad884cfd3d552976add6535d913da8733a26a
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-08-05 18:16:21 +0200

    refactor: move convert function to utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const convertSnakeCaseToCamelCase = (str: string) => {
+  return str.replace(/([-_][a-z])/g, (group) =>
+    group.toUpperCase().replace("-", "").replace("_", "")
+  );
+}
