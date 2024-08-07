bfbc69c33bc774e86ab3f55e93da34db476be712
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-08-07 13:58:30 +0200

    chore: Update Jest configuration to ignore test files in node_modules and utils directory

--- jest.config.ts
+++ jest.config.ts
@@ -13,6 +13,7 @@ const config: Config = {
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   setupFiles: ["<rootDir>/jest.setup.ts"],
+  testPathIgnorePatterns: ["/node_modules/", "/utils/"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
