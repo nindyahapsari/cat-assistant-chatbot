2c376d75c9dba685f55df30f2d67f04ab4a15566
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-08-07 13:58:40 +0200

    chore: Refactor Sidebar.test.tsx and add test-utils for testing

--- __tests__/Sidebar.test.tsx
+++ __tests__/Sidebar.test.tsx
@@ -1,12 +1,8 @@
-import "@testing-library/jest-dom"; // Import the toBeInTheDocument function
-import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { screen } from "@testing-library/react";
+import { renderWithProviders } from "./utils/test-utils";
 import Sidebar from "@/components/Sidebar";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ClerkProvider } from "@clerk/nextjs";
-import { MessagesProvider } from "@/context/messages";
 import { SIDEBARLINKSINFO } from "@/components/Sidebar";
-
-const queryClient = new QueryClient();
 
 //source: stackoverflow
 jest.mock("next/navigation", () => {
@@ -25,21 +21,6 @@ jest.mock("next/navigation", () => {
     }),
   };
 });
-
-const clerkPubKey =
-  process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY || "not found";
-
-function renderSidebarWithProvider() {
-  return render(
-    <QueryClientProvider client={queryClient}>
-      <MessagesProvider>
-        <ClerkProvider publishableKey={clerkPubKey}>
-          <Sidebar>Test Content</Sidebar>
-        </ClerkProvider>
-      </MessagesProvider>
-    </QueryClientProvider>
-  );
-}
 
 describe("SIDEBARLINKSINFO", () => {
   it("contains the correct links", () => {
@@ -60,17 +41,15 @@ describe("SIDEBARLINKSINFO", () => {
 
 describe("Sidebar Component", () => {
   it("renders the Sidebar component", () => {
-    renderSidebarWithProvider();
+    // renderSidebarWithProvider();
+    renderWithProviders(<Sidebar>Test Content</Sidebar>);
 
-    // Check if the logo is rendered
     expect(screen.getByAltText("logo")).toBeInTheDocument();
 
-    // Check if the sidebar links are rendered
     SIDEBARLINKSINFO.forEach((link) => {
       expect(screen.getByText(link.name)).toBeInTheDocument();
     });
 
-    // Check if the children are rendered
     expect(screen.getByText("Test Content")).toBeInTheDocument();
   });
 });
